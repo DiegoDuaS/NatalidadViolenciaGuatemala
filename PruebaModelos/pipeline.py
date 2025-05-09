a907b952a1fdabe420ebd7cc9a6eb121300f31c3
DiegoDuaS <diego.slowing@gmail.com>
2025-05-08 20:40:09 -0600

    feat: added more pipelines

--- PruebaModelos/pipeline.py
+++ PruebaModelos/pipeline.py
@@ -9,3 +9,35 @@ def crear_pipeline_regresion(modelo):
         ('model', modelo)
     ])
     return pipeline
+
+def crear_pipeline_knn(modelo):
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler()),
+        ('model', modelo)
+    ])
+    return pipeline
+
+def crear_pipeline_svm(modelo):
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler()),
+        ('model', modelo)
+    ])
+    return pipeline
+
+def crear_pipeline_regresion_lineal(modelo):
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler()), 
+        ('model', modelo)
+    ])
+    return pipeline
+
+def crear_pipeline_random_forest(modelo):
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('model', modelo)
+    ])
+    return pipeline
+
